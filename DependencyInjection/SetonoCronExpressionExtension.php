b06230067023764624c8e7648fbf72bb99b6b26a
Joachim Løvgaard <joachim@loevgaard.dk>
2018-09-26 11:43:23 +0200

    added orm config

--- DependencyInjection/SetonoCronExpressionExtension.php
+++ DependencyInjection/SetonoCronExpressionExtension.php
@@ -7,9 +7,10 @@ namespace Setono\CronExpressionBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class SetonoCronExpressionExtension extends Extension
+final class SetonoCronExpressionExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -20,5 +21,28 @@ final class SetonoCronExpressionExtension extends Extension
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @throws \Exception
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        if(!$container->hasExtension('doctrine')) {
+            throw new \Exception('The doctrine extension was not loaded');
+        }
+
+        $container->prependExtensionConfig('doctrine', [
+            'orm' => [
+                'mappings' => [
+                    'SetonoCronExpressionBundle' => [
+                        'type' => 'xml',
+                        'prefix' => 'Cron'
+                    ]
+                ]
+            ]
+        ]);
     }
 }
