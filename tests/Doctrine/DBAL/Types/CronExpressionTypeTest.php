9b37a982b26aa1cfe2a12a13ba9a54e845760e8f
Hans Mackowiak <h.mackowiak@b3-it.de>
2022-06-07 13:30:01 +0200

    more tests for DBAL/Types/CronExpressionType (#34)
    * more tests for DBAL/Types/CronExpressionType
    
    * ~ fixes
    
    * ~ remove @throws

--- tests/Doctrine/DBAL/Types/CronExpressionTypeTest.php
+++ tests/Doctrine/DBAL/Types/CronExpressionTypeTest.php
@@ -6,14 +6,61 @@ namespace Setono\CronExpressionBundle\Tests\Doctrine\DBAL\Types;
 
 use Cron\CronExpression;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Types\Type;
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
 use Setono\CronExpressionBundle\Doctrine\DBAL\Types\CronExpressionType;
+use stdClass;
 
 final class CronExpressionTypeTest extends TestCase
 {
-    use ProphecyTrait;
+    /**
+     * @test
+     */
+    public function testTypeName(): void
+    {
+        self::assertEquals('cron_expression', $this->getType()->getName());
+    }
+
+    /**
+     * @test
+     */
+    public function testTypeRequiresHint(): void
+    {
+        self::assertTrue($this->getType()->requiresSQLCommentHint($this->getPlatform()));
+    }
+
+    /**
+     * @test
+     */
+    public function testTypeColumn(): void
+    {
+        $length = 255;
+        $sql = $this->getType()->getSQLDeclaration([
+            'length' => $length
+        ], $this->getPlatform());
+        self::assertStringContainsString(strval($length), $sql);
+    }
+
+    /**
+     * @test
+     */
+    public function convertToPhpNull(): void
+    {
+        $val = $this->getType()->convertToPHPValue(null, $this->getPlatform());
+
+        self::assertNull($val);
+    }
+
+    /**
+     * @test
+     */
+    public function convertEmptyToPhpNull(): void
+    {
+        $val = $this->getType()->convertToPHPValue('', $this->getPlatform());
+
+        self::assertNull($val);
+    }
 
     /**
      * @test
@@ -28,11 +75,40 @@ final class CronExpressionTypeTest extends TestCase
     /**
      * @test
      */
+    public function convertFaultyTypeToPhpThrowsException(): void
+    {
+        self::expectException(ConversionException::class);
+
+        $this->getType()->convertToPHPValue(new stdClass(), $this->getPlatform());
+    }
+
+    /**
+     * @test
+     */
+    public function convertFaultyStringToPhpThrowsException(): void
+    {
+        self::expectException(ConversionException::class);
+
+        $this->getType()->convertToPHPValue('@never', $this->getPlatform());
+    }
+
+    /**
+     * @test
+     */
     public function convertToDatabaseReturnsString(): void
     {
         $val = $this->getType()->convertToDatabaseValue(CronExpression::factory('0 0 * * *'), $this->getPlatform());
 
         self::assertSame('0 0 * * *', $val);
+    }
+
+    /**
+     * @test
+     */
+    public function convertToDatabaseNull(): void
+    {
+        $val = $this->getType()->convertToDatabaseValue(null, $this->getPlatform());
+        self::assertNull($val);
     }
 
     private function getType(): CronExpressionType
@@ -49,6 +125,19 @@ final class CronExpressionTypeTest extends TestCase
 
     private function getPlatform(): AbstractPlatform
     {
-        return $this->prophesize(AbstractPlatform::class)->reveal();
+        $mock = $this->createMock(AbstractPlatform::class);
+        $mock->method('getVarcharTypeDeclarationSQL')
+            ->withAnyParameters()
+            ->willReturnCallback(function (array $column)
+        {
+            /** @var int $length */
+            $length = $column['length'];
+            /** @var bool $fixed */
+            $fixed = $column['fixed'] ?? false;
+
+            return $fixed ? ($length > 0 ? 'CHAR(' . $length . ')' : 'CHAR(254)')
+                : ($length > 0 ? 'VARCHAR(' . $length . ')' : 'VARCHAR(255)');
+        });
+        return $mock;
     }
 }
