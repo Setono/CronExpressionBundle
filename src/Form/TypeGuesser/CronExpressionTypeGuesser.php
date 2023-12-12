cedfb46702aa7ec497565e4c73f6e20035979659
Hans Mackowiak <h.mackowiak@b3-it.de>
2023-12-12 15:11:40 +0100

    * use phpDocumentor as backup

--- src/Form/TypeGuesser/CronExpressionTypeGuesser.php
+++ src/Form/TypeGuesser/CronExpressionTypeGuesser.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Setono\CronExpressionBundle\Form\TypeGuesser;
 
 use Cron\CronExpression;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+use phpDocumentor\Reflection\DocBlockFactory;
+use phpDocumentor\Reflection\Types\ContextFactory;
+use phpDocumentor\Reflection\Types\Object_;
 use Setono\CronExpressionBundle\Form\Type\CronExpressionType;
 use Symfony\Component\Form\FormTypeGuesserInterface;
 use Symfony\Component\Form\Guess\Guess;
@@ -15,9 +19,9 @@ use Symfony\Component\PropertyInfo\Type;
 
 final class CronExpressionTypeGuesser implements FormTypeGuesserInterface
 {
-    protected PropertyTypeExtractorInterface $extractor;
+    private ?PropertyTypeExtractorInterface $extractor;
 
-    public function __construct(PropertyTypeExtractorInterface $extractor)
+    public function __construct(?PropertyTypeExtractorInterface $extractor = null)
     {
         $this->extractor = $extractor;
     }
@@ -31,14 +35,51 @@ final class CronExpressionTypeGuesser implements FormTypeGuesserInterface
             return null;
         }
 
-        $types = $this->extractor->getTypes($class, $property);
-        if (!$types) {
-            return null;
-        }
-        foreach ($types as $type) {
-            if (Type::BUILTIN_TYPE_OBJECT === $type->getBuiltinType() &&
-                CronExpression::class === $type->getClassName()) {
-                return new TypeGuess(CronExpressionType::class, [], Guess::VERY_HIGH_CONFIDENCE);
+        if ($this->extractor) {
+            $types = $this->extractor->getTypes($class, $property);
+            if (!$types) {
+                return null;
+            }
+            foreach ($types as $type) {
+                if (Type::BUILTIN_TYPE_OBJECT === $type->getBuiltinType() &&
+                    CronExpression::class === $type->getClassName()) {
+                    return new TypeGuess(CronExpressionType::class, [], Guess::VERY_HIGH_CONFIDENCE);
+                }
+            }
+        } else {
+            // NEXT MAJOR: Remove Compatible Layer
+            try {
+                $reflectionClass = new \ReflectionClass($class);
+                $reflectionProperty = $reflectionClass->getProperty($property);
+                if ($reflectionProperty->hasType()) {
+                    $type = $reflectionProperty->getType();
+
+                    if ($type->getName() === CronExpression::class) {
+                        return new TypeGuess(CronExpressionType::class, [], Guess::VERY_HIGH_CONFIDENCE);
+                    }
+                } else {
+                    $docBlockFactory = DocBlockFactory::createInstance();
+                    $contextFactory = new ContextFactory();
+
+                    $docBlock = $docBlockFactory->create($reflectionProperty,
+                        $contextFactory->createFromReflector($reflectionProperty));
+
+                    foreach ($docBlock->getTagsByName('var') as $tag) {
+                        /**
+                         * @var $tag Var_
+                         */
+                        $tagType = $tag->getType();
+                        if ($tagType instanceof Object_) {
+                            $fqsen = $tagType->getFqsen();
+                            $fqsen = ltrim((string)$fqsen, '\\');
+                            if (CronExpression::class === $fqsen) {
+                                return new TypeGuess(CronExpressionType::class, [], Guess::VERY_HIGH_CONFIDENCE);
+                            }
+                        }
+                    }
+                }
+            } catch (\ReflectionException $e) {
+                return null;
             }
         }
         return null;
---
+++ tests/Form/TypeGuesser/CronExpressionTypeGuesserWithoutPropertyInfoTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\CronExpressionBundle\Tests\Form\TypeGuesser;
+
+use PHPUnit\Framework\TestCase;
+use Setono\CronExpressionBundle\Form\Type\CronExpressionType;
+use Setono\CronExpressionBundle\Form\TypeGuesser\CronExpressionTypeGuesser;
+use Symfony\Component\Form\Guess\Guess;
+use Symfony\Component\Form\Guess\TypeGuess;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
+
+final class CronExpressionTypeGuesserWithoutPropertyInfoTest extends TestCase
+{
+    private CronExpressionTypeGuesser $typeGuesser;
+
+    public function setUp(): void
+    {
+        $this->typeGuesser = new CronExpressionTypeGuesser();
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_null_if_class_does_not_exist(): void
+    {
+        $this->assertNull($this->typeGuesser->guessType('Class\\Does\\Not\\Exist', 'property'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_null_if_no_phpdoc_is_present(): void
+    {
+        $this->assertNull($this->typeGuesser->guessType(StubWithNoPhpDoc::class, 'property'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_null_if_property_doesnt_exist(): void
+    {
+        $this->assertNull($this->typeGuesser->guessType(StubWithNoPhpDoc::class, 'property2'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_null_if_property_has_wrong_type(): void
+    {
+        $this->assertNull($this->typeGuesser->guessType(StubWithWrongType::class, 'property'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_guesses_type_when_type_is_a_fqcn(): void
+    {
+        $this->guess_type(StubFqcn::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_guesses_type_when_type_is_an_alias(): void
+    {
+        $this->guess_type(StubAliased::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_guesses_type_when_type_is_imported(): void
+    {
+        $this->guess_type(StubImported::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_guesses_type_when_type_is_hinted(): void
+    {
+        $this->guess_type(StubWithTypeHint::class);
+    }
+
+    protected function guess_type(string $class): void
+    {
+        $this->assertCorrectGuess($this->typeGuesser->guessType($class, 'property'));
+        $this->assertNull($this->typeGuesser->guessRequired($class, 'property'));
+        $this->assertNull($this->typeGuesser->guessMaxLength($class, 'property'));
+        $this->assertNull($this->typeGuesser->guessPattern($class, 'property'));
+    }
+
+    private function assertCorrectGuess(?TypeGuess $res): void
+    {
+        $this->assertNotNull($res);
+        $this->assertSame(CronExpressionType::class, $res->getType());
+        $this->assertSame(Guess::VERY_HIGH_CONFIDENCE, $res->getConfidence());
+    }
+}
