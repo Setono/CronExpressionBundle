ab7a7691f43567a1253229050a5e86cbbd2f4055
Hans Mackowiak <h.mackowiak@b3-it.de>
2025-12-01 14:23:47 +0100

    @phpstan-ignore staticMethod.dynamicCall

--- tests/Form/Type/CronExpressionTypeCallbackTest.php
+++ tests/Form/Type/CronExpressionTypeCallbackTest.php
@@ -29,6 +29,7 @@ final class CronExpressionTypeCallbackTest extends TestCase
     protected function callValidateCronField(?string $value, bool $match): void
     {
         $mock = $this->createMock(ExecutionContextInterface::class);
+        /** @phpstan-ignore staticMethod.dynamicCall */
         $mock->expects($match ? $this->once() : $this->never())->method('addViolation')
             ->with('{{value}} is not a valid cron part', ['value' => $value])
         ;
